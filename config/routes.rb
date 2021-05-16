bce51823186827e6eb838bf676f9c51797329c3a
Sagar GUPTA <guptazy@gmail.com>
2021-05-16 20:04:29 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'home#index'
+
   devise_for :users,
     path: 'account',
     path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' },
@@ -9,7 +11,18 @@ Rails.application.routes.draw do
       registrations: 'users/registrations',
       passwords: 'users/passwords'
     }
-  resources :maps
 
+  authenticated do
+    root to: 'dashboard#index', as: :authenticated_root
+  end
+
+  resources :dashboard, only: [:index]
+
+  scope module: 'dashboard' do
+    resources :maps
+    resources :locations
+  end
+
+  # Tmp
   get '/maps_test' => 'maps#maps_test'
 end
