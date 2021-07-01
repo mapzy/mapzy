4d200a87a3479862d345206e635da9b496a9f3b5
Sagar Gupta <guptazy@gmail.com>
2021-07-01 20:05:37 +0200

    Fix ability specs

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -9,14 +9,14 @@ describe Ability, type: :model do
   let(:location) { create(:location, map: map) }
 
   context 'when not logged in' do
-    subject(:ability) { Ability.new }
+    subject(:ability) { Ability.new(nil) }
 
     context 'with maps (read-only)' do
       it { is_expected.to be_able_to(:read, map) }
 
       it { is_expected.not_to be_able_to(:create, Map.new(user: owner)) }
       it { is_expected.not_to be_able_to(:update, map) }
-      it { is_expected.to be_able_to(:destroy, location) }
+      it { is_expected.not_to be_able_to(:destroy, map) }
     end
 
     context 'with locations (read-only)' do
@@ -50,34 +50,19 @@ describe Ability, type: :model do
     subject(:ability) { Ability.new(fraud) }
 
     context 'with maps' do
+      it { is_expected.to be_able_to(:read, map) }
+
       it { is_expected.not_to be_able_to(:create, Map.new(user: owner)) }
-      it { is_expected.not_to be_able_to(:read, map) }
       it { is_expected.not_to be_able_to(:update, map) }
       it { is_expected.not_to be_able_to(:destroy, map) }
     end
 
     context 'with locations' do
+      it { is_expected.to be_able_to(:read, location) }
+
       it { is_expected.not_to be_able_to(:create, Location.new(map: map)) }
-      it { is_expected.not_to be_able_to(:read, location) }
       it { is_expected.not_to be_able_to(:update, location) }
       it { is_expected.not_to be_able_to(:destroy, location) }
     end
   end
-
-  #   context 'with locations' do
-  #     it { is_expected.to be_able_to(:manage, Location.new(map: map)) }
-  #   end
-  # end
-
-  # context 'when forbidden' do
-  #   let(:hacker) { described_class.new }
-
-  #   context 'with maps' do
-  #     it { is_expected.not_to be_able_to(:manage, Map.new) }
-  #   end
-
-  #   context 'with locations' do
-  #     it { is_expected.not_to be_able_to(:manage, Location.new(map: map)) }
-  #   end
-  # end
 end
