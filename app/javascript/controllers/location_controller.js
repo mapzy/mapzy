17dc3d627ff117738f626d26cc631d1ac0fd8a14
Sagar Gupta <guptazy@gmail.com>
2021-07-10 17:13:04 +0200

    Update geocoder logic

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -1,25 +1,36 @@
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
 
 export default class extends Controller {
   static targets = [
     "address",
     "zipCode",
     "city",
-    "state",
     "country",
     "lat",
     "lng",
-    "geocoder"
   ]
 
   static values = {
     "mapboxAccessToken": String
   }
 
+  get fullAddress() {
+    const zipCodeCity = [this.zipCodeTarget.value, this.cityTarget.value].join(' ')
+    return [this.addressTarget.value, zipCodeCity, this.countryTarget.value].join(',');
+  }
+
+  isReadyToGeocode() {
+    return (
+      this.addressTarget.value &&
+      this.zipCodeTarget.value &&
+      this.cityTarget.value &&
+      this.countryTarget.value
+    )
+  }
+
   initialize() {
-    this.initMapbox()
-    // this.initNewMarker()
-    this.addGeocoder()
+    this.initMapbox();
+    this.initMapboxSdk();
   }
 
   initMapbox() {
@@ -33,57 +44,61 @@ export default class extends Controller {
     });
   }
 
-  // initNewMarker() {
-  //   this.marker = new mapboxgl.Marker({
-  //     draggable: true
-  //   })
-  //   .setLngLat([13.419506188839986, 52.4942909415001])
-  //   .addTo(this.map);
+  initMapboxSdk() {
+    this.mapboxClient = mapboxSdk({ accessToken: this.mapboxAccessTokenValue });
+  }
 
-  //   this.marker.on('dragend', () => this.onDragEnd())
-  // }
-
-  addMarker(lat, lng) {
+  addMarker(center) {
     this.marker = new mapboxgl.Marker({
       draggable: true
     })
-    .setLngLat([lng, lat])
+    .setLngLat(center)
     .addTo(this.map);
 
-    this.marker.on('dragend', () => this.onDragEnd())
-  }
-
-  addGeocoder() {
-    this.geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl,
-    })
-
-    this.geocoder.on('result', () => this.onGeocoderResult())
-
-    this.geocoderTarget.appendChild(this.geocoder.onAdd(this.map));
+    this.marker.on('dragend', () => this.onDragEnd());
   }
 
   onDragEnd() {
     const lngLat = this.marker.getLngLat(); 
 
-    console.log(this.marker)
-
     this.latTarget.value = lngLat.lat;
     this.lngTarget.value = lngLat.lng;
   }
 
-  onGeocoderResult() {
-    console.log(this.geocoder)
-    console.log(this.geocoder.lastSelected)
-    console.log(this.geocoder.lastSelected["center"])
-    console.log(JSON.parse(this.geocoder.lastSelected))
-    console.log(JSON.parse(this.geocoder.lastSelected)["center"])
-  }
+  forwardGeocode() {
+    if (this.isReadyToGeocode()) {
+      setTimeout(() => {
+        const query = this.fullAddress;
 
-  searchByAddress() {
-    if (this.addressLine1Target) {
-      this.forwardGeocode(this.addressLine1Target)
+        this.mapboxClient.geocoding
+          .forwardGeocode({
+            query: query,
+            autocomplete: false,
+            limit: 1
+          })
+          .send()
+          .then((response) => {
+            if (
+              response &&
+              response.body &&
+              response.body.features &&
+              response.body.features.length
+            ) {
+                const feature = response.body.features[0];
+
+                this.addMarker(feature.center);
+
+                this.map.flyTo({
+                  center: feature.center,
+                  zoom: 13,
+                  bearing: 0,
+                  essential: true
+                })
+
+            }
+          });
+
+      }, 3000)
     }
   }
 }
