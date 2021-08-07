35c2a2db0c1e1b26c647e6c258b740eef2937e58
Sagar Gupta <guptazy@gmail.com>
2021-08-07 19:55:25 +0200

    Absolute position & dismiss after 15 sec

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,6 +3,10 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ["message"]
 
+  connect() {
+    setTimeout(() => this.dismiss(), 15000);
+  }
+
   dismiss() {
     this.messageTarget.classList.add("hidden");
   }
