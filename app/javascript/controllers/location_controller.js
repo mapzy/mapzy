82b0fd2f435a5f51822f456312ac6dce15a0bedc
Sagar Gupta <guptazy@gmail.com>
2021-09-17 23:35:45 +0200

    Add autocompletion on search, revisit UI (WIP)

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = [
     "address",
+    "geocoder",
     "zipCode",
     "city",
     "country",
@@ -19,22 +20,15 @@ export default class extends Controller {
     "bounds": Array,
   }
 
-  get fullAddress() {
-    const zipCodeCity = [this.zipCodeTarget.value, this.cityTarget.value].join(' ')
-    return [this.addressTarget.value, zipCodeCity, this.countryTarget.value].join(',');
-  }
-
   get adjustMarkerBlockHidden() {
     return this.adjustMarkerBlockTarget.classList.contains("hidden");
   }
 
   initialize() {
     this.initMapbox();
-    this.initMapboxSdk();
     this.initMarker();
-    this.showAdjustMarkerLink();
-
-    this.typingIntervalValue = 1000;
+    this.initGeocoder();
+    this.handleAdjustMarkerLink();
   }
 
   initMapbox() {
@@ -45,10 +39,30 @@ export default class extends Controller {
       style: 'mapbox://styles/mapbox/streets-v11',
       bounds: this.boundsValue,
     });
+
+    this.mapboxClient = mapboxSdk({ accessToken: this.mapboxAccessTokenValue });
   }
 
-  initMapboxSdk() {
-    this.mapboxClient = mapboxSdk({ accessToken: this.mapboxAccessTokenValue });
+  initGeocoder() {
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken
+    });
+
+    geocoder.addTo(this.geocoderTarget);
+
+    // When geocoder result is selected by user
+    geocoder.on('result', (e) => {
+      console.log(JSON.stringify(e.result, null, 2));
+
+      this.moveMarker(e.result.center);
+      this.updateLngLat(e.result.center[0], e.result.center[1]);
+      this.handleAdjustMarkerLink();
+    });
+
+    // When geocoder is cleared
+    geocoder.on('clear', () => {
+      //
+    });
   }
 
   initMarker() {
@@ -63,6 +77,16 @@ export default class extends Controller {
     }
   }
 
+  onDragEnd() {
+    const lngLat = this.marker.getLngLat();
+    this.updateLngLat(lngLat.lng, lngLat.lat);
+  }
+
+  updateLngLat(lng, lat) {
+    this.longitudeTarget.value = lng;
+    this.latitudeTarget.value = lat;
+  }
+
   moveMarker(center) {
     this.marker.setLngLat(center);
     this.marker.addTo(this.map);
@@ -73,63 +97,7 @@ export default class extends Controller {
     this.map.flyTo({
       center: center,
       zoom: 15,
-      bearing: 0,
-      essential: true
-    });
-  }
-
-  onDragEnd() {
-    const lngLat = this.marker.getLngLat(); 
-
-    this.latitudeTarget.value = lngLat.lat;
-    this.longitudeTarget.value = lngLat.lng;
-  }
-
-  isReadyToGeocode() {
-    return (
-      this.addressTarget.value &&
-      this.zipCodeTarget.value &&
-      this.cityTarget.value &&
-      this.countryTarget.value
-    )
-  }
-
-  // Wait until the user has (probably) finished typing the address fields
-  searchByAddress() {
-    clearTimeout(this.typingTimerValue);
-    if (this.isReadyToGeocode()) {
-      this.typingTimerValue = setTimeout(() => this.forwardGeocode(), this.typingIntervalValue);
-    }
-  }
-
-  forwardGeocode() {
-    const query = this.fullAddress;
-
-    this.mapboxClient.geocoding
-    .forwardGeocode({
-      query: query,
-      autocomplete: false,
-      limit: 1
-    })
-    .send()
-    .then((response) => {
-      if (
-        response &&
-        response.body &&
-        response.body.features &&
-        response.body.features.length
-      ) {
-        const feature = response.body.features[0];
-
-        this.moveMarker(feature.center);
-
-        this.longitudeTarget.value = feature.center[0];
-        this.latitudeTarget.value = feature.center[1];
-
-        if (this.adjustMarkerBlockHidden) {
-          this.showAdjustMarkerLink();
-        }
-      }
+      bearing: 5
     });
   }
 
@@ -144,14 +112,76 @@ export default class extends Controller {
     }
   }
 
-  showAdjustMarkerLink() {
-    if (this.latitudeTarget.value && this.longitudeTarget.value) {
-      this.adjustMarkerLinkTarget.classList.remove("hidden");
+  handleAdjustMarkerLink() {
+    if (this.latitudeTarget.value && this.longitudeTarget.value && this.adjustMarkerBlockHidden) {
+      this.showAdjustMarkerLink();
+    } else {
+      this.hideAdjustMarkerLink();
     }
   }
 
-  showAdjustMarkerBlock() {
+  hideAdjustMarkerLink() {
     this.adjustMarkerLinkTarget.classList.add("hidden");
-    this.adjustMarkerBlockTarget.classList.remove("hidden");
   }
+
+  showAdjustMarkerLink() {
+    this.adjustMarkerLinkTarget.classList.remove("hidden");
+  }
+
+  showAdjustMarkerBlock() {
+    this.adjustMarkerBlockTarget.classList.remove("hidden");
+    this.handleAdjustMarkerLink();
+  }
+
+  // get fullAddress() {
+  //   const zipCodeCity = [this.zipCodeTarget.value, this.cityTarget.value].join(' ')
+  //   return [this.addressTarget.value, zipCodeCity, this.countryTarget.value].join(',');
+  // }
+
+  // isReadyToGeocode() {
+  //   return (
+  //     this.addressTarget.value &&
+  //     this.zipCodeTarget.value &&
+  //     this.cityTarget.value &&
+  //     this.countryTarget.value
+  //   )
+  // }
+
+  // Wait until the user has (probably) finished typing the address fields
+  // searchByAddress() {
+  //   clearTimeout(this.typingTimerValue);
+  //   if (this.isReadyToGeocode()) {
+  //     this.typingTimerValue = setTimeout(() => this.forwardGeocode(), this.typingIntervalValue);
+  //   }
+  // }
+
+  // forwardGeocode() {
+  //   const query = this.fullAddress;
+
+  //   this.mapboxClient.geocoding
+  //   .forwardGeocode({
+  //     query: query,
+  //     autocomplete: false,
+  //     limit: 1
+  //   })
+  //   .send()
+  //   .then((response) => {
+  //     if (
+  //       response &&
+  //       response.body &&
+  //       response.body.features &&
+  //       response.body.features.length
+  //     ) {
+  //       const feature = response.body.features[0];
+
+  //       this.moveMarker(feature.center);
+  //       updateLngLat(feature.center[0], feature.center[1]);
+
+  //       if (this.adjustMarkerBlockHidden) {
+  //         this.showAdjustMarkerLink();
+  //       }
+  //     }
+  //   });
+  // }
+
 }
