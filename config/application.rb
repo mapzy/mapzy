18853ec466fc27706981c4985f20eaa5f5c064ae
Sagar Gupta <guptazy@gmail.com>
2021-09-17 16:26:47 +0200

    Add time zone Europe/Zurich

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Mapzy
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Europe/Zurich"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Use Rspec as test framework
