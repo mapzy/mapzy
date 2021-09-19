8fd78d2a6b40211ad2d482b768b4a565bc5f0e29
Sagar Gupta <guptazy@gmail.com>
2021-09-19 17:46:16 +0200

    Migration: remove city, zip code, country_code, state

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_122314) do
+ActiveRecord::Schema.define(version: 2021_09_19_151147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,15 +19,11 @@ ActiveRecord::Schema.define(version: 2021_09_17_122314) do
     t.string "name"
     t.text "description"
     t.string "address"
-    t.string "city"
-    t.string "zip_code"
-    t.string "country_code"
     t.decimal "latitude", precision: 15, scale: 10
     t.decimal "longitude", precision: 15, scale: 10
     t.bigint "map_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "state"
     t.index ["map_id"], name: "index_locations_on_map_id"
   end
 
