9e3b7bc2b9b2d52c3c2f51b6c6e773240e355c19
Sagar Gupta <guptazy@gmail.com>
2021-11-04 16:11:17 +0100

    Linting

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -48,12 +48,12 @@ RSpec.describe Location, type: :model do
 
     before do
       new_address_stub = [new_address, [{
-        "latitude"     => 40.7143528,
-        "longitude"    => -74.0059731,
-        "address"      => "New York, NY, USA",
-        "state"        => "New York",
-        "state_code"   => "NY",
-        "country"      => "United States",
+        "latitude" => 40.7143528,
+        "longitude" => -74.0059731,
+        "address" => "New York, NY, USA",
+        "state" => "New York",
+        "state_code" => "NY",
+        "country" => "United States",
         "country_code" => "US"
       }]]
 
