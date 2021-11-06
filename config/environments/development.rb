6bd8d294e8a5db864621b7537d32d4659f5667a9
Sagar Gupta <guptazy@gmail.com>
2021-11-06 23:17:41 +0100

    Remove yarn integrity in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Do not verify yarn integrity
+  config.webpacker.check_yarn_integrity = false
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
