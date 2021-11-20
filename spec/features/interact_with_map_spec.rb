805a1c7302d3abab3e9ae40fd3691e3ebe6732e5
Can Olcer <canolcer@hey.com>
2021-11-20 15:46:41 +0100

    Fix little things (#25)

--- spec/features/interact_with_map_spec.rb
+++ spec/features/interact_with_map_spec.rb
@@ -10,15 +10,6 @@ RSpec.describe "interacting with the map", type: :feature do
     visit map_path(map.id)
   end
 
-  it "shows permission box" do
-    expect(page).to have_selector(:link_or_button, "Grant permission")
-  end
-
-  it "hides permission box on hide click" do
-    page.find_button("Hide").click
-    expect(page).not_to have_selector(:link_or_button, "Grant permission")
-  end
-
   it "does not show location name before click" do
     expect(page).not_to have_content(location.name)
   end
