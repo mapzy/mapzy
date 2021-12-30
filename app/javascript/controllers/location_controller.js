1c7703f3c56d57ba528b5722b21e656218101890
Sagar Gupta <guptazy@gmail.com>
2021-12-30 16:17:47 +0100

    Fix marker

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -92,8 +92,7 @@ export default class extends Controller {
   initMarker() {
     this.marker = new mapboxgl.Marker({
       draggable: true,
-      color: "#E74D67",
-      anchor: "bottom"
+      color: "#E74D67"
     });
 
     this.marker.on('dragend', () => this.onDragEnd());
