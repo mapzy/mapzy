c7bb92b8b49b8190ae5e8735a0a3613a08eaa6f6
Sagar Gupta <guptazy@gmail.com>
2021-12-30 18:15:09 +0100

    Preview emails in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,8 +47,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Use letter opener in development
-  # config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
