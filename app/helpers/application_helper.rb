ef8a4467cea6fdb66e799288d47e53354ea6f082
Can Olcer <canolcer@hey.com>
2022-04-10 16:48:32 +0000

    fix: moved helpers to same file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def mapzy_cloud?
+    ENV["MAPZY_CLOUD"] == "true"
+  end
+
   def allow_registration?
     ENV["ALLOW_REGISTRATION"] == "true"
   end
--- app/helpers/cloud_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module CloudHelper
-  def mapzy_cloud?
-    ENV["MAPZY_CLOUD"] == "true"
-  end
-end
