00035f55bd11e12169dcb4dbd30d9da7afc72c1e
Sagar Gupta <guptazy@gmail.com>
2022-04-25 15:16:44 +0200

    Change batch strategy

--- app/workers/batch_geocode_worker.rb
+++ app/workers/batch_geocode_worker.rb
@@ -7,16 +7,18 @@ class BatchGeocodeWorker
     map = Map.find(map_id)
     return if map.nil?
 
-    locations_to_geocode = map.locations.where(geocoding_status: "pending")
+    locations_to_geocode = map.locations.geocoding_pending
     return if locations_to_geocode.empty?
 
-    locations_to_geocode.each do |location|
-      location.geocode
-      location.save
+    # Mapbox Geocoding limit is 600 per minute, we geocode max. 500 locations per batch
+    # @see https://docs.mapbox.com/api/search/geocoding/#geocoding-restrictions-and-limits
+    locations_to_geocode.find_in_batches(batch_size: 500) do |group|
+      group.each do |location|
+        location.geocode
+        location.save
+      end
 
-      # Mapbox Geocoding limit is 10 per second
-      # @see https://docs.mapbox.com/api/search/geocoding/#geocoding-restrictions-and-limits
-      sleep(0.15)
+      sleep(60)
     end
   end
 end
