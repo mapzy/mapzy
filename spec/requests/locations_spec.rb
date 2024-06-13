73a6cdb9ada974460e06fcdb893fcd5a7e809598
Can Olcer <canolcer@hey.com>
2024-06-13 13:18:56 +0200

    fixed linting

--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe "Locations", type: :request do
   let(:map) { create(:map) }
   let(:location) { create(:location, :with_opening_times, map_id: map.id) }
-  #let(:opening_time) { create(:opening_time, day: 1, location: location ) }
+  # let(:opening_time) { create(:opening_time, day: 1, location: location ) }
 
   describe "GET locations/show" do
     context "with no language param" do
@@ -22,19 +22,18 @@ RSpec.describe "Locations", type: :request do
       end
     end
 
-    context "with no language param" do
+    context "with language param" do
       before do
-        get map_location_path(map.hashid, location.hashid), params: {language: "de"}
+        get map_location_path(map.hashid, location.hashid), params: { language: "de" }
       end
 
       it "responds with a HTTP 200" do
         expect(response).to be_successful
       end
 
-       it "contains opening times german" do
+      it "contains opening times german" do
         expect(response.body).to include("Montag")
       end
     end
-
   end
 end
